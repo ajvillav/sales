7d98c7f06871432cdc0af84baf4a961c2ddeba9c
unknown <ajvillav@unal.edu.co>
2023-08-26 14:16:47 -0500

    Create Data folder and DataContext class in Sales.Backend, create Entities folder and Country class in Shared project

---
+++ Sales.Backend/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sales.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Country")]
+        [MaxLength(100, ErrorMessage = "The field {0} cannot have more than {1} characters!")]
+        [Required(ErrorMessage = "The field {0} is mandatory!")]
+        public string Name { get; set; } = null!;
+    }
+}
